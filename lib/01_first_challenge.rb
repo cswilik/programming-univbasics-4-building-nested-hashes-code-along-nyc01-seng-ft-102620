abe0188301565c4eed0e3a74277540e75ccbf26e
Chelsey <cswilik@gmail.com>
2020-10-10 21:49:25 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,6 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :Montague => { :patriarch => {:name => "Lord Montague", :age =>  "53"}, :matriarch => {:name => "Lady Montague", :age => "54"}, :hero => {:name => romeo, :age => "15"}, :heroes_friends [ {:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}] },
+     :capulet => {:patriarch => { :name => "Lord Capulet", :age => "50"}, :matriarch => { :name => "Lady Capulet", :age =>"51"}, :heroine => {:name => "Juliet", :age => "15"
+   }
 end
