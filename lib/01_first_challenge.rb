af729eac570f5da19cc855e4b87c556deae0b67b
Chelsey <cswilik@gmail.com>
2020-10-10 22:05:13 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,15 +1,20 @@
 def first_challenge
    epic_tragedy = {
-     :Montague => 
-     { :patriarch => {:name => "Lord Montague", :age =>  "53"}, :matriarch => {:name => "Lady Montague", :age => "54"}, 
-         :hero => {:name => "Romeo", :age => "15", :status => "Alive"}, 
-       :heroes_friends => [ {:name => "Benvolio", :age => "17", :attitude => "worried"}, 
-                         {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}] },
+     :Montague => { 
+      :patriarch => {:name => "Lord Montague", :age =>  "53"},
+      :matriarch => {:name => "Lady Montague", :age => "54"}, 
+      :hero => {:name => "Romeo", :age => "15", :status => "Alive"}, 
+      :heroes_friends => [ {:name => "Benvolio", :age => "17", :attitude => "worried"}, 
+                           {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
+                          ]
+                    },
      :Capulet => {
       :patriarch => { :name => "Lord Capulet", :age => "50"},
-      :matriarch => { :name => "Lady Capulet", :age =>"51"}, :heroine => {:name => "Juliet", :age => "15", :status => "Alive"} 
-        :heroines_friends => [ {:name => "Steven", :age => "30", :attitude => "confused"}, 
-                            {:name => "Nurse", :age => "44", :attitude => "worried"} ]
-      }
-  }
+      :matriarch => { :name => "Lady Capulet", :age =>"51"},
+      :heroine => {:name => "Juliet", :age => "15", :status => "Alive"},
+      :heroines_friends => [ {:name => "Steven", :age => "30", :attitude => "confused"}, 
+                            {:name => "Nurse", :age => "44", :attitude => "worried"} 
+                            ]
+                    }
+}
 end
